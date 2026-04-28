67b98e4b0ea96f1701cefbaafa0557c18ffbbf75
josiasbechtel <josias.bechtel@dialekta.ch>
2026-04-28 20:42:07 +0200

    Add clean template aggregator

---
+++ app/clean-template/index.ts
@@ -0,0 +1,7 @@
+import chunk1 from './chunk1';
+import chunk2 from './chunk2';
+import chunk3 from './chunk3';
+import chunk4 from './chunk4';
+import chunk5 from './chunk5';
+
+export const workspaceTemplateGzipBase64 = [chunk1, chunk2, chunk3, chunk4, chunk5].join('');
