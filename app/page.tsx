804ae061db2054beffa7b49d14bd5a655b284485
josiasbechtel <josias.bechtel@dialekta.ch>
2026-04-28 20:47:36 +0200

    Remove build-time template decoding

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { gunzipSync } from 'node:zlib';
 import HandwerkWorkspace from '../components/HandwerkWorkspace';
-import { workspaceTemplateGzipBase64 } from './clean-template';
-
-function decodeWorkspaceHtml() {
-  const buffer = Buffer.from(workspaceTemplateGzipBase64, 'base64');
-  return gunzipSync(buffer).toString('utf-8');
-}
 
 export default function Page() {
-  const initialHtml = decodeWorkspaceHtml();
-  return <HandwerkWorkspace initialHtml={initialHtml} />;
+  return <HandwerkWorkspace />;
 }
